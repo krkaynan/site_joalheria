f744885f3c4d64d27bb99a21669675e4aa851dee
Kaynan Raikkonen <raikkonenkaynan@gmail.com>
2025-10-15 15:44:04 -0300

    Adicionando catigorias

--- joalheria/views.py
+++ joalheria/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from store.models import Product
+from decimal import Decimal, ROUND_HALF_UP
 
 # Criando os links das paginas html
 def home(request):
     products = Product.objects.all().filter(is_available = True)
+    
+    # calcula a parcela (10x sem juros) para cada produto
+    for p in products:
+        try:
+            price = Decimal(str(p.price))
+            p.installment = (price / Decimal('10')).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+        except Exception:
+            p.installment = None
     
     context = {
         'products': products,
